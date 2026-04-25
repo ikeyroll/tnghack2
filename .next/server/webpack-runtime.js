e3a3e222ae6c0ea4b9b9d7ac441697258f4a2b3b
Muhammad Muadz Jamain <muadz.jamain@gmail.com>
2026-04-25 18:31:55 +0800

    merge

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("45b9fcc36cf3033e")
+/******/ 		__webpack_require__.h = () => ("360b70935426f6e6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\MikhailShahmieBinSha\Desktop\TnGHack\tnghack2\app\layout.tsx
+// File: C:\Users\muadz\Desktop\tngHack\tnghack2\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
